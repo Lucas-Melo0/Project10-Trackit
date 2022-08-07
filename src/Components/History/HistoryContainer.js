955cab2fe0831911e361b9e90257cb6b712b290c
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-07 19:31:33 -0300

    [feat]: Added a feature to list habits of the day on calendar

--- src/Components/History/HistoryContainer.js
+++ src/Components/History/HistoryContainer.js
@@ -6,13 +6,16 @@ import { useState, useEffect, useContext } from "react";
 import { getHabitsHistory } from "../../API/sendData";
 import { UserContext } from "../UserContext";
 import dayjs from 'dayjs';
+import { FaCheckSquare } from "react-icons/fa";
+import { AiFillCloseCircle } from "react-icons/ai";
 
 
 export default function HistoryContainer() {
-
+    
     const [date, setDate] = useState(new Date());
     const [history, setHistory] = useState([]);
     const { userInfo } = useContext(UserContext);
+    const [selectedHabit, setSelectedHabit] = useState([]);
 
 
     useEffect(() => {
@@ -33,18 +36,43 @@ export default function HistoryContainer() {
             }
         }
     }
+    function showHabitsOfDay(dates) {
+        const selectedDate = dayjs(new Date(dates)).format('DD/MM/YYYY');
+        const matchingDate = history.filter((value) => value.day === selectedDate);
+        if (matchingDate.length === 1) {
+            setSelectedHabit([
+            {
+                date: matchingDate[0].day,
+                habits: matchingDate[0].habits.map((value) => value)
+            }])
+        }
+        console.log(history)
+        console.log(selectedHabit)
+        console.log(matchingDate)
+        console.log(selectedHabit)
+    }
 
     return (
         <Wrapper>
             <Container>
                 <H3>Histórico</H3>
                 {
-                    history.length === 0 ? <Loader/> : <Calendar
-                    onChange={setDate}
-                    value={date}
-                    tileClassName={({ date }) => tileClass(history, dayjs(new Date(date)).format('DD/MM/YYYY'))}/>
+                    history.length === 0 ? <Loader /> : <Calendar
+                        onClickDay={showHabitsOfDay}
+                        onChange={setDate}
+                        value={date}
+                        tileClassName={({ date }) => tileClass(history, dayjs(new Date(date)).format('DD/MM/YYYY'))} />
                 }
-                
+
+                {
+                    selectedHabit.length > 0 ? <> <H3>Hábitos do dia</H3>
+                        <H3>{selectedHabit[0].date}</H3>
+                        {selectedHabit[0].habits.map((habit)=>{
+                            return <DayContainer>{habit.name} {habit.done ? <FaCheckSquare color={"#8FC549"}/> : <AiFillCloseCircle color={"#EA5766"}/>} </DayContainer>
+                        })} </> : null
+                    
+                    }
+
             </Container>
         </Wrapper>
 
@@ -82,6 +110,17 @@ const Container = styled.div`
        
     }
    
+`
+const DayContainer = styled.div `
+    display: flex;
+    height: 50px;
+    padding: 10px 10px;
+    justify-content:space-between;
+    align-items: center;
+    width: 90vw;
+    background-color: #FFFFFF;
+    font-size: 20px;
+
 `
 const H3 = styled.h3`
     font-family: 'Lexend Deca', sans-serif;
