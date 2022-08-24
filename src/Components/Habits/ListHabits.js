0de61728b3af83066c6c6e341ae5701314bbc746
Lucas-Melo0 <lucassss8v@gmail.com>
2022-08-24 10:20:13 -0300

    [fix]: Fixed a delay when deleting habits

--- src/Components/Habits/ListHabits.js
+++ src/Components/Habits/ListHabits.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 import { weekLetters } from "../../auxiliary/days";
-import { FaTrash } from 'react-icons/fa';
+import { FaTrash } from "react-icons/fa";
 import { deleteHabit, getHabits } from "../../API/sendData";
 import { UserContext } from "../UserContext";
 import { useState, useEffect } from "react";
@@ -8,86 +8,80 @@ import { useContext } from "react";
 import Loader from "../Loader/Loader";
 import { WeekButtonContainer, WeekButton } from "./Habits";
 export default function ListHabits() {
+  const { userInfo, render, setRender } = useContext(UserContext);
+  const [habitsData, setHabitsData] = useState([]);
 
-    const { userInfo, render, setRender } = useContext(UserContext);
-    const [habitsData, setHabitsData] = useState([]);
+  useEffect(() => {
+    getHabits(userInfo.token)
+      .catch((value) => console.log(value))
+      .then((value) => setHabitsData(value.data.reverse()));
+  }, [render]);
 
-
-    useEffect(() => {
-        getHabits(userInfo.token)
-            .catch((value) => console.log(value))
-            .then((value) => setHabitsData(value.data.reverse()));
-        
-    }, [render]);
-
-  
-   
-    function habitDelete(value) {
-        if (window.confirm("Deseja realmente apagar ?")) {
-            deleteHabit(value.id, userInfo.token);
-            setRender(render + 1);
-
-        }
+  function habitDelete(value) {
+    if (window.confirm("Deseja realmente apagar ?")) {
+      deleteHabit(value.id, userInfo.token).then(() => setRender(render + 1));
     }
-    
-    
-    return (
+  }
+
+  return (
+    <>
+      {habitsData.length === 0 ? (
+        <NoHabitsParagraph>
+          Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para
+          começar a trackear!
+        </NoHabitsParagraph>
+      ) : (
         <>
-            {
-                habitsData.length === 0 ? <NoHabitsParagraph>Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!</NoHabitsParagraph> 
-                : <> 
-                    {
-                        habitsData.map((value, index) => {
-                            return (
-
-                                <HabitsCard key={index}>
-                                    <p>{value.name}</p>
-                                    <FaTrash onClick={() => habitDelete(value)} />
-                                    <WeekButtonContainer>
-                                        {
-                                            weekLetters.map((letters, index) => <WeekButton selected={value.days.includes(index + 1) || (value.days.includes(index - 6))} key={index}>{letters}</WeekButton>)
-                                        }
-                                    </WeekButtonContainer>
-                                </HabitsCard>
-                            )
-                        })
-                    }
-
-                </>
-            }
-
+          {habitsData.map((value, index) => {
+            return (
+              <HabitsCard key={index}>
+                <p>{value.name}</p>
+                <FaTrash onClick={() => habitDelete(value)} />
+                <WeekButtonContainer>
+                  {weekLetters.map((letters, index) => (
+                    <WeekButton
+                      selected={
+                        value.days.includes(index + 1) ||
+                        value.days.includes(index - 6)
+                      }
+                      key={index}
+                    >
+                      {letters}
+                    </WeekButton>
+                  ))}
+                </WeekButtonContainer>
+              </HabitsCard>
+            );
+          })}
         </>
-    )
-
-
+      )}
+    </>
+  );
 }
 
-
 const HabitsCard = styled.div`
-    row-gap: 10px;
-    display: flex;
-    flex-direction: column;
-    width: 90vw;
-    padding: 10px 20px;
-    height: 90px;
-    background: #FFFFFF;
-    border-radius: 5px;
-    position: relative;
-    p {
-        font-size: 20px;
-        color: #666666;
-    }
-    svg{
-        cursor: pointer;
-        fill:#666666;
-        position: absolute;
-        right: 10px;
-    }
-   
-`
-const NoHabitsParagraph = styled.p`
-    font-size: 18px;
+  row-gap: 10px;
+  display: flex;
+  flex-direction: column;
+  width: 90vw;
+  padding: 10px 20px;
+  height: 90px;
+  background: #ffffff;
+  border-radius: 5px;
+  position: relative;
+  p {
+    font-size: 20px;
     color: #666666;
-    font-family: 'Lexend Deca', sans-serif;
-
-`
+  }
+  svg {
+    cursor: pointer;
+    fill: #666666;
+    position: absolute;
+    right: 10px;
+  }
+`;
+const NoHabitsParagraph = styled.p`
+  font-size: 18px;
+  color: #666666;
+  font-family: "Lexend Deca", sans-serif;
+`;
